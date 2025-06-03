e24604ed80feb9a3543159023476d49ec1ace82a
NCCMNT <chmielewski2003@gmail.com>
2025-06-03 16:31:29 +0200

    added teamup mechanic

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,9 @@
-use std::mem;
-
 use anyhow::Context as _;
 use poise::serenity_prelude::{ClientBuilder, GatewayIntents};
 use serenity::{all::{ChannelId, CreateEmbed, GuildId, Member}};
 use shuttle_runtime::SecretStore;
 use shuttle_serenity::ShuttleSerenity;
 use rand::{self, seq::{IndexedRandom, SliceRandom}};
-use rand::SeedableRng;
-use rand_chacha::ChaChaRng;
 
 struct Data {}
 type Error = Box<dyn std::error::Error + Send + Sync>;
@@ -23,51 +19,63 @@ async fn greeting(ctx: Context<'_>) -> Result<(), Error> {
 #[poise::command(slash_command)]
 async fn teamup(
     ctx: Context<'_>,
-    #[description = "Comma-separated list of voice channels for teams"] vc_teams_raw: String,
+    #[description = "Comma-separated list of voice channels for teams"] 
+    // #[channel_types("Voice")]
+    // channels: Vec<serenity::all::Channel>,
+    channels: String
+
 ) -> Result<(), Error> {
     // get guild attributes
-    let guild = ctx.guild().ok_or("Command must be used in the server")?;
     let guild_id = ctx.guild_id().ok_or("Command must be used in the server")?;
     let author = ctx.author();
     let voice_channel_id = ctx
         .guild()
         .and_then(|g| g.voice_states.get(&author.id)?.channel_id)
-        .ok_or("Command can be used only when you are on a voice channel")?;
+        .ok_or("Command must be used in the server")?;
     
-    // parse input string into vc team list
-    let vc_teams: Vec<String> = vc_teams_raw.split(',').map(|s| s.trim().to_string()).collect();
-    
-    // get number of teams for users to be splitted to
-    let number_of_teams = vc_teams.len();
-    
-    // get voice channels by String
-    let mut voice_channels_teams = vec![];
-    for vc_team in vc_teams {
-        let voice_channel_team = guild.channels.values()
-        .find(|channel| {
-                channel.kind == serenity::model::channel::ChannelType::Voice &&
-                channel.name == vc_team
-            })
-            .ok_or_else(|| format!("Voice channel '{}' not found", vc_team))?;
-        
-        voice_channels_teams.push(voice_channel_team);
-    }
-
-    // get channel members on caller's voice channel
+    // get channel members
     let mut channel_members = get_channel_members(guild_id, voice_channel_id, ctx).await?;
-
+    
     // use only real members, exclude bots
     channel_members.retain(|member| !member.user.bot);
 
-    // get number of members
+    // get all voice channels
+    let voice_channels = get_voice_channels(guild_id, ctx).await?;
+    
+    // get voice channels by String
+    let vc_names: Vec<_> = channels
+        .split(',')
+        .map(|s| s.trim())
+        .filter(|s| !s.is_empty())
+        .collect();
+
+    // get number of teams for users to be splitted to
+    let number_of_teams = vc_names.len();
+    if number_of_teams <= 1 {
+        return Err("Need at least two teams to perfom teamup.".into());
+    }
+
+    let mut voice_channels_teams = vec![];
+    for name in vc_names {
+        let voice_channel_team = voice_channels
+            .iter()
+            .find(|channel| channel.name == *name)
+            .ok_or_else(|| format!("Voice channel '{}' not found", name))?;
+        
+        voice_channels_teams.push(voice_channel_team.id);
+    }
+
+    // // get number of members
     let number_of_members = channel_members.len();
     if number_of_members <= 1 {
         return Err("Need at least two members in the voice channel to perfom teamup.".into());
     }
 
     // shuffle randomly channel members
-    let mut rng = ChaChaRng::from_entropy();
-    channel_members.shuffle(&mut rng);
+    {
+        let mut rng = rand::rng();
+        channel_members.shuffle(&mut rng);
+    };
 
     // perform teamup
     let mut teams: Vec<Vec<Member>> = vec![vec![]; number_of_teams];
@@ -78,7 +86,7 @@ async fn teamup(
     
     // distribute team members to voice channels
     for (i, team) in teams.iter().enumerate() {
-        let team_voice_channel = voice_channels_teams[i].id;
+        let team_voice_channel = voice_channels_teams[i];
         for member in team {
             member.move_to_voice_channel(ctx.serenity_context(), team_voice_channel).await?;
         }
@@ -166,12 +174,30 @@ async fn get_channel_members(
         .guild(guild_id)
         .ok_or("Guild not found")?;
 
-    Ok(guild.voice_states.values()
+    let members: Vec<Member> = guild.voice_states.values()
         .filter(|voice_state| voice_state.channel_id == Some(voice_channel_id))
         .filter_map(|voice_state| guild.members.get(&voice_state.user_id))
         .cloned()
-        .collect()
-    )
+        .collect();
+
+    Ok(members)
+}
+
+async fn get_voice_channels(
+    guild_id: poise::serenity_prelude::GuildId,
+    ctx: Context<'_>
+) -> Result<Vec<poise::serenity_prelude::GuildChannel>, Error> {
+    let guild = ctx.cache()
+        .guild(guild_id)
+        .ok_or("Guild not found")?;
+
+    let channels = guild.channels
+        .values()
+        .filter(|channel| channel.kind == serenity::model::channel::ChannelType::Voice)
+        .cloned()
+        .collect();
+
+    Ok(channels)
 }
 
 #[shuttle_runtime::main]
@@ -209,7 +235,9 @@ async fn main(#[shuttle_runtime::Secrets] secret_store: SecretStore) -> ShuttleS
     let intents = GatewayIntents::non_privileged()
         | GatewayIntents::GUILD_VOICE_STATES
         | GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::MESSAGE_CONTENT;
+        | GatewayIntents::MESSAGE_CONTENT
+        | GatewayIntents::GUILD_VOICE_STATES
+        | GatewayIntents::GUILD_MEMBERS;
 
     let client = ClientBuilder::new(discord_token, intents)
         .framework(framework)
